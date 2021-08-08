306f8c7b195d90dc4cddc1db6ac87946cee37378
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-08 11:32:06 -0300

    sim pl type system definition

---
+++ simpl_with_type_system/simpl_with_type_system.ml
@@ -0,0 +1,93 @@
+(*
+Type Checking
+
+A type system is a mathematical description to determine
+whether an expression is ill typed or well typed,
+and in the latter case, what the type of the expression is.
+A type checker is a program that implements a type system, i.e.,
+that implements the static semantics of the language.
+
+Commonly, a type system is formulated as ternary relation
+HasType(Γ, expr, type), which means that expression expr
+has type type in typing context Γ. A typing context, aka
+typing environment, is a map from identifiers to types.
+The context is used to record what variables are in scope,
+and what their types are. The use of the Greek Latter Γ(Gamma)
+for contexts is traditional.
+
+The ternary relation HasType is typically written with infix
+notation, though, as Γ ⊢ expr : type
+
+Recall the syntax of SimPL:
+
+expr :=
+    x
+  | integer
+  | bool 
+  | expr1 binop expr2
+  | if expr1 then expr2 else expr3
+  | let x = expr1 in expr2
+
+binop ::=
+    +
+  | *
+  | <=
+
+Let's define a type system Γ ⊢ expr : type for SimPL.
+The only types in SimPL are integers and booleans.
+
+type ::=
+    integer
+  | bool
+
+Base Cases
+
+An integer constant has type int in any context whatsoever,
+a Boolean constant likewise always has type bool, and
+a variable has whatever type the context says it should have.
+Here are the typing rules that express those ideas:
+
+Γ ⊢ integer : int -- integer always have type int
+Γ ⊢ boolean : bool -- booleans always have type bool
+{x: type1, ...} ⊢ x : type1 -- a variable has whatever type the
+context says it has.
+
+Inductive Cases
+
+Let expressions
+
+Γ ⊢ let x = expr1 in expr2 : type2
+  if Γ ⊢ expr1 : type1
+  and Γ[x -> type1] ⊢ expr2 : type2
+
+The rule says that let x = expr1 in expr2 has type type2
+in context Γ, but only if certain conditions hold.
+The first condition is that expr1 has type type1 in context Γ.
+The second is that expr2 has type type2 in a new context,
+which is Γ extended to bind x to type1
+
+Binary operators
+
+Γ ⊢ expr1 binop expr1 : int
+  if binop is +
+  and Γ ⊢ expr1 : int
+  and Γ ⊢ expr2 : int
+
+Γ ⊢ expr1 binop expr2 : int
+  if binop is *
+  and Γ ⊢ expr1 : int
+  and Γ ⊢ expr2 : int
+
+Γ ⊢ expr1 binop expr2 : bool
+  if binop is <=
+  and Γ ⊢ expr1 : int
+  and Γ ⊢ expr2 : int
+
+If expressions
+
+Γ ⊢ if expr1 then expr2 else expr3 : type1
+  if Γ ⊢ expr1 : bool
+  and Γ ⊢ expr2 : type1
+  and Γ ⊢ expr3 : type1
+*)
+let () = print_endline "Hello, World!"
